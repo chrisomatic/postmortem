1ecc6ee512a0ad43d163fde892590f2f0ecb613a
kameron <kameronmpenn@gmail.com>
2022-10-27 13:11:45 -0400

    Moved console stuff to gui

--- gui.c
+++ gui.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
+
 #include "main.h"
 #include "window.h"
 #include "gfx.h"
@@ -10,6 +11,7 @@
 #include "gui.h"
 #include "world.h"
 #include "camera.h"
+#include "zombie.h"
 
 void gui_draw()
 {
@@ -26,8 +28,8 @@ void gui_draw()
     {
         float scale = 0.12;
 
-        Vector2f size = gfx_string_get_size(scale, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890|[](){}");
-        // Vector2f size = gfx_string_get_size(scale, "j");
+        // Vector2f size = gfx_string_get_size(scale, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890|[](){}");
+        Vector2f size = gfx_string_get_size(scale, "x");
 
 
         float yspace = size.y*1.5;
@@ -51,12 +53,6 @@ void gui_draw()
             gfx_draw_string(x, y, console_msg[i].color, scale, 0.0, 1.0, false, false, "%s", console_msg[i].msg);// y += size.y+ypad;
         }
 
-
-
-        // float y0 = tbox.y-tbox.h/1.5/2.0;
-
-        // printf("console_text[0] = %d\n", (int)console_text[0]);
-        // const char* prompt = "cmd > ";
         Vector2f psize = gfx_string_get_size(scale, (char*)CONSOLE_PROMPT);
 
         int index = 0;
@@ -180,5 +176,234 @@ void gui_draw()
         float fps = timer_get_prior_frame_fps(&game_timer);
         size = gfx_string_get_size(scale, "fps: %.2f", fps);
         gfx_draw_string(view_width-size.x,0,0x00FFFF00,scale,0.0, 1.0, false,drop_shadow,"fps: %.2f", fps);
+    }
+}
+
+
+void console_message_add(uint32_t color, char* fmt, ...)
+{
+    // 0 is oldest
+    if(console_msg_count >= CONSOLE_MSG_MAX)
+    {
+        for(int i = 1; i < CONSOLE_MSG_MAX; ++i)
+        {
+            memcpy(&console_msg[i-1], &console_msg[i], sizeof(ConsoleMessage));
+        }
+    }
+
+    int index = MIN(console_msg_count, CONSOLE_MSG_MAX-1);
+
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(console_msg[index].msg, CONSOLE_TEXT_MAX+1, fmt, args);
+    va_end(args);
+
+    console_msg[index].color = color;
+    console_msg_count = MIN(console_msg_count+1, CONSOLE_MSG_MAX);
+    // for(int i = 0; i < console_msg_count; ++i)
+    // {
+    //     printf("%d) %s\n", i, console_msg[i].msg);
+    // }
+}
+
+void console_text_hist_add(char* text)
+{
+    console_message_add(COLOR_WHITE, "%s%s", CONSOLE_PROMPT, text);
+
+    if(!STR_EQUAL(text, console_text_hist[console_text_hist_index]))
+    {
+        int next_index = console_text_hist_index+1;
+        if(next_index >= CONSOLE_HIST_MAX) next_index = 0;
+        memset(console_text_hist[next_index], 0, CONSOLE_TEXT_MAX);
+        memcpy(console_text_hist[next_index], text, strlen(text));
+        console_text_hist_index = next_index;
+        // printf("added to index: %d\n", next_index);
+    }
+}
+
+// gets next non-empty index
+int console_text_hist_get(int direction)
+{
+    int index = console_text_hist_selection;
+    if(index == -1)
+    {
+        index = console_text_hist_index;
+        if(direction == -1)
+            index += 1;
+    }
+
+    // printf("start search at index: %d\n", index);
+    for(int i = 0; i < CONSOLE_HIST_MAX; ++i)
+    {
+        index += direction;
+        if(index < 0) index = CONSOLE_HIST_MAX-1;
+        if(index >= CONSOLE_HIST_MAX) index = 0;
+        if(!STR_EMPTY(console_text_hist[index]))
+            return index;
+    }
+    return -1;
+}
+
+//TODO: some of these commands should only work locally
+void run_console_command(char* text)
+{
+    if(STR_EMPTY(text))
+        return;
+
+    LOGI("parse command: '%s'", text);
+
+    console_text_hist_add(text);
+    console_text_hist_selection = -1;
+
+
+    int cmd_len = 0;
+    char* cmd = string_split_index(text, " ", 0, &cmd_len, true);
+
+    // this is the case if there's a space before the command
+    if(cmd_len == 0) return;
+
+    // char* arg0 = string_split_index_copy(text, " ", 1, true);
+    // char* args = string_split_index_copy(text, " ", 1, false);
+    // printf("arg0: %s\n", arg0 == NULL ? "NULL" : arg0);
+    // printf("args: %s\n", args == NULL ? "NULL" : args);
+    // if(arg0 != NULL) free(arg0);
+    // if(args != NULL) free(args);
+
+    // printf("cmdlen: %d\n", cmd_len);
+    LOGI("  cmd: '%.*s'", cmd_len, cmd);
+
+    if(STRN_EQUAL(cmd,"exit",cmd_len))
+    {
+        // printf("closing\n");
+        window_set_close(1);
+    }
+    else if(STRN_EQUAL(cmd,"setname",cmd_len))
+    {
+        // setname <name>
+        // char* name = cmd+cmd_len+1; //+1 for space delimiter
+        char* name = string_split_index_copy(text, " ", 1, false);
+        if(name != NULL)
+        {
+            memset(player->name, 0, PLAYER_NAME_MAX);
+            memcpy(player->name, name, MIN(strlen(name),PLAYER_NAME_MAX));
+            FREE(name);
+        }
+    }
+    else if(STRN_EQUAL(cmd,"teleport",cmd_len) || STRN_EQUAL(cmd,"tp",cmd_len))
+    {
+        // teleport <row> <col>
+        char* s_row = string_split_index_copy(text, " ", 1, true);
+        char* s_col = string_split_index_copy(text, " ", 2, true);
+
+        if(s_row == NULL || s_col == NULL)
+        {
+            FREE(s_row);
+            FREE(s_col);
+            return;
+        }
+
+        int row = atoi(s_row);
+        int col = atoi(s_col);
+
+        row = RANGE(row, 0, map.rows-1);
+        col = RANGE(col, 0, map.cols-1);
+        // printf("row, col: %d, %d\n", row, col);
+        float x,y;
+        map_grid_to_coords(row, col, &x, &y);
+        player->phys.pos.x = x;
+        player->phys.pos.y = y;
+
+        FREE(s_row);
+        FREE(s_col);
+    }
+    else if(STRN_EQUAL(cmd,"goto",cmd_len))
+    {
+        // goto <object> <index>
+
+        char* s_object = string_split_index_copy(text, " ", 1, true);
+        char* s_index = string_split_index_copy(text, " ", 2, true);
+
+        if(s_object == NULL || s_index == NULL)
+        {
+            FREE(s_object);
+            FREE(s_index);
+            return;
+        }
+
+        int idx = atoi(s_index);
+
+        if(STR_EQUAL(s_object,"zombie"))
+        {
+            if(idx < zlist->count)
+            {
+                // printf("goto zombie %d\n", idx);
+                player->phys.pos.x = zombies[idx].phys.pos.x;
+                player->phys.pos.y = zombies[idx].phys.pos.y;
+            }
+        }
+        else if(STR_EQUAL(s_object,"player"))
+        {
+            // printf("goto player %d (my index: %d)\n", idx, player->index);
+            if(idx != player->index && idx < MAX_CLIENTS)
+            {
+                if(players[idx].active)
+                {
+                    player->phys.pos.x = players[idx].phys.pos.x;
+                    player->phys.pos.y = players[idx].phys.pos.y;
+                }
+            }
+        }
+
+        FREE(s_object);
+        FREE(s_index);
+    }
+    else if(STRN_EQUAL(cmd,"spawn",cmd_len))
+    {
+        // spawn <object> <row> <col>
+        char* s_object = string_split_index_copy(text, " ", 1, true);
+        if(s_object == NULL)
+        {
+            FREE(s_object);
+            return;
+        }
+
+        int row, col;
+        coords_to_map_grid(player->phys.pos.x, player->phys.pos.y, &row, &col);
+
+        char* s_row = string_split_index_copy(text, " ", 2, true);
+        char* s_col = string_split_index_copy(text, " ", 3, true);
+        if(s_row != NULL && s_col != NULL)
+        {
+            row = atoi(s_row);
+            col = atoi(s_col);
+        }
+
+        float x, y;
+        map_grid_to_coords(row, col, &x, &y);
+
+        if(STR_EQUAL(s_object,"zombie"))
+        {
+            ZombieSpawn spawn = {0};
+            spawn.pos.x = x;
+            spawn.pos.y = y;
+            zombie_add(&spawn);
+        }
+        else if(STR_EQUAL(s_object,"player"))
+        {
+            for(int i = 0; i < MAX_CLIENTS; ++i)
+            {
+                if(!players[i].active)
+                {
+                    players[i].phys.pos.x = x;
+                    players[i].phys.pos.y = y;
+                    players[i].active = true;
+                    break;
+                }
+            }
+        }
+
+        FREE(s_object);
+        FREE(s_row);
+        FREE(s_col);
     }
 }
