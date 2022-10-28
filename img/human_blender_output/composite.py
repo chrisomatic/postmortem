bf025366ed4147eda6a9d64fd59a37f24613e7ff
kameron <kameronmpenn@gmail.com>
2022-10-28 11:28:40 -0400

    small changes to composite script

--- img/human_blender_output/composite.py
+++ img/human_blender_output/composite.py
@@ -5,9 +5,11 @@ import sys, os
 import math
 import struct
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QKeyEvent, QPainter,QImage, QPen, QIcon, QPixmap, QColor, QBrush, QCursor, QFont, QPalette, QTransform, QLinearGradient, QFontMetrics, QStaticText
+from PyQt5.QtGui import QKeyEvent, QPainter,QImage, QPen, QIcon, QPixmap, QColor, QBrush, QCursor, QFont, QPalette, QTransform, QLinearGradient, QFontMetrics, QStaticText, qRgba
 from PyQt5.QtCore import Qt, QPoint, QPointF, QSize, QEvent, QTimer, QCoreApplication, QRect
 import copy
+import numpy as np
+
 
 slash = "/"
 if sys.platform == "win32":
@@ -30,12 +32,22 @@ class Compositor(QWidget):
         super().__init__()
 
 
-        self.scaled_size = QSize(64, 128)
+        
         self.color_clear = QColor(0,0,0,0)
         self.color_rep_clear = QColor(255,0,255,255)
-        self.root = root = os.path.dirname(os.path.abspath(__file__)) + "/"
+        # self.color_rep_clear = QColor(0,0,0,255)
 
-        pics = [x.lower() for x in os.listdir(self.root) if os.path.isfile(self.root+x) and x.lower().endswith(".png") and x.lower().startswith("000")]
+        # self.color_clear = qRgba(0,0,0,0)
+        # self.color_rep_clear = QColor(0,0,0,255)
+
+        self.root = root = os.path.dirname(os.path.abspath(__file__)) + slash
+
+        self.path = self.root + "standing" + slash
+        self.path = self.root + "walk1" + slash
+
+        self.scaled_size = QSize(64, 128)
+
+        pics = [x.lower() for x in os.listdir(self.path) if os.path.isfile(self.path+x) and x.lower().endswith(".png") and x.lower().startswith("000")]
         pics.sort()
 
         if(len(pics) == 0):
@@ -47,9 +59,11 @@ class Compositor(QWidget):
         self.pic_data = {}
         for i in range(len(pics)):
             p = pics[i]
-            printf("Loading image %s (%d of %d)      \r", p, i+1, len(pics))
+            # printf("Loading image %s (%d of %d)      \r", p, i+1, len(pics))
+            printf("Loading image %s (%d of %d)      \n", p, i+1, len(pics))
             num = int(p.replace(".png",""))
-            img = QImage(self.root+p,"PNG")
+            img = QImage(self.path+p,"PNG")
+            img = img.convertToFormat(QImage.Format_ARGB32)
             img,cnt = self.sub_img_color(img, self.color_rep_clear, self.color_clear)
             w = img.width()
             h = img.height()
@@ -63,7 +77,7 @@ class Compositor(QWidget):
             self.pic_data[p]["img"] = img
             self.pic_data[p]["img_scaled"] = img_scaled
 
-        printf("Loaded %d image(s)                                                 \n", len(self.pic_data))
+        # printf("Loaded %d image(s)                                                 \n", len(self.pic_data))
 
         self.keys = [x for x in self.pic_data.keys()]
         self.krange = range(len(self.keys))
@@ -72,16 +86,36 @@ class Compositor(QWidget):
         QCoreApplication.instance().quit()
 
     def sub_img_color(self, img, find, replace):
+
+        am = img.createAlphaMask()
+        # am.fill(1)
+        am.fill(0)
+        ptr = img.bits()
+        ptr.setsize(img.byteCount())
+        ## copy the data out as a string
+        strData = ptr.asstring()
+        ## get a read-only buffer to access the data
+        buf = memoryview(ptr)
+        ## view the data as a read-only numpy array
+        arr = np.frombuffer(buf, dtype=np.ubyte).reshape(img.height(), img.width(), 4)
+        ## view the data as a writable numpy array
+        arr = np.asarray(ptr).reshape(img.height(), img.width(), 4)
+
+
         count = 0
         for y in range(img.height()):
             for x in range(img.width()):
                 color = QColor(img.pixel(x,y))
                 if(color == find):
-                    img.setPixelColor(x,y,replace)
+                    # img.setPixelColor(x,y,replace)
+                    arr[y][x][3] = 0
+                    color = QColor(img.pixel(x,y))
                     count += 1
+        # img.setAlphaChannel(am)
         return img,count
 
     def draw_png(self):
+        # return
         # print("draw png")
         tw = 0
         for i in self.krange:
@@ -90,8 +124,12 @@ class Compositor(QWidget):
 
         image = QImage(tw, self.scaled_size.height(), QImage.Format_ARGB32)
 
+        # painter = QPainter(image)
+
         painter = QPainter()
         painter.begin(image)
+
+        # painter.setCompositionMode(QPainter.CompositionMode_Source)
 
         x = 0
         for i in self.krange:
@@ -101,15 +139,14 @@ class Compositor(QWidget):
             x += img.width()
 
         painter.end()
-        path = self.root+"composite.png"
-        image.save(path)
+        path = self.path+"composite_test.png"
+        image.save(path,"PNG")
         printf("Saved: %s\n", path)
 
     def paintEvent(self, event):
         print("paint event")
         self.draw_png()
         QCoreApplication.instance().quit()
-
 
 
 
